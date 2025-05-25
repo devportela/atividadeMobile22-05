b1c9bb666db72a857b733163dbcc1296653b2808
gustavo portela <portelagustavo947@gmail.com>
2025-05-25 10:23:53 -0300

    atividade finalizada

--- app/src/main/java/com/example/etimpamigustavoportelapreferenciasdecores/MainActivity.kt
+++ app/src/main/java/com/example/etimpamigustavoportelapreferenciasdecores/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.etimpamigustavoportelapreferenciasdecores
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.etimpamigustavoportelapreferenciasdecores.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private val NOME_ARQUIVO = "minhas_preferencias"
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +47,47 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    override fun onResume() {
+        super.onResume()
+        val preferencias = getSharedPreferences(NOME_ARQUIVO, MODE_PRIVATE)
+        val cor = preferencias.getString("cor", "")
+        if (cor!!.isNotEmpty()) {
+            binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+        }
+
+    }
+
+
     private fun salvarCor(cor: String) {
         binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+        binding.btnTrocar.setOnClickListener { view ->
+            val preferencias = getSharedPreferences(NOME_ARQUIVO, MODE_PRIVATE)
+            val editor = preferencias.edit()
+            editor.putString("cor", cor)
+            editor.putString("nome", "Gustavo")
+            editor.putString("sobrenome", "Portela")
+            editor.putInt("idade", 90)
+            editor.apply()
+
+            snackBar(view)
+        }
+
     }
+
+    private fun snackBar(view: View) {
+        val snackbar =
+            Snackbar.make(view, "cor de fundo alterada com sucesso!", Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("OK") {
+
+        }
+        snackbar.setActionTextColor(Color.BLUE)
+        snackbar.setBackgroundTint(Color.LTGRAY)
+        snackbar.setTextColor(Color.GREEN)
+        snackbar.show()
+
+
+    }
+
+
 }
